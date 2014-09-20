128c222916055657153d1a7d330022db19120bcb
frank <frankjdelgado@gmail.com>
2014-09-19 22:50:39 -0430

    template tweaks, devise flash messages errors

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+
+	def devise_flash
+		if controller.devise_controller? && resource.errors.any?
+			messages = resource.errors.full_messages
+			flash.now[:error] = messages.uniq!
+			flash.now[:error] = messages.join(", ")
+		end
+	end
+	
 end
