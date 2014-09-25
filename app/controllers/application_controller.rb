1b809cc1008636bd299905f5db99722b35eeddb5
frank <frankjdelgado@gmail.com>
2014-09-25 03:04:58 -0430

    change lang

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,12 @@ class ApplicationController < ActionController::Base
 
 	before_action :configure_permitted_parameters, if: :devise_controller?
 
+	before_action :set_locale
+
+	def set_locale
+	  I18n.locale = params[:locale]
+	end
+
 	protected
 
 	def configure_permitted_parameters
